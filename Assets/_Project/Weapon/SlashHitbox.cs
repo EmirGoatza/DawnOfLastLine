ba9fb707b9687880cd69efa982d82f3edcae3913
Konaten <anthony.bove07@gmail.com>
2026-02-12 12:49:55 +0100

    fix slash hitbox duration

--- Assets/_Project/Weapon/SlashHitbox.cs
+++ Assets/_Project/Weapon/SlashHitbox.cs
@@ -1,14 +1,40 @@
 using UnityEngine;
+using System.Collections; // Nécessaire pour IEnumerator
 using System.Collections.Generic;
 
 public class SlashHitbox : MonoBehaviour
 {
+    [Header("Durée de la hitbox")]
+    [Tooltip("Temps en secondes avant que la zone de dégâts ne se désactive.")]
+    public float activeDuration = 0.1f;
+
     private int damageAmount;
-    private List<GameObject> hitEnemies = new List<GameObject>(); 
+    private List<GameObject> hitEnemies = new List<GameObject>();
+    private Collider myCollider;
+
+    void Awake()
+    {
+        myCollider = GetComponent<Collider>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(DisableColliderAfterTime());
+    }
 
     public void Setup(int damage)
     {
         damageAmount = damage;
+    }
+
+    IEnumerator DisableColliderAfterTime()
+    {
+        yield return new WaitForSeconds(activeDuration);
+        
+        if (myCollider != null)
+        {
+            myCollider.enabled = false;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,10 +57,18 @@ public class SlashHitbox : MonoBehaviour
         
         if (box != null)
         {
-            Gizmos.color = new Color(1, 0, 0, 0.4f);
+            if (!box.enabled) 
+            {
+                Gizmos.color = new Color(0.5f, 0.5f, 0.5f, 0.2f);
+            }
+            else 
+            {
+                Gizmos.color = new Color(1, 0, 0, 0.4f);
+            }
+
             Gizmos.matrix = transform.localToWorldMatrix;
             Gizmos.DrawCube(box.center, box.size);
-            Gizmos.color = Color.red;
+            Gizmos.color = box.enabled ? Color.red : Color.gray;
             Gizmos.DrawWireCube(box.center, box.size);
         }
     }
