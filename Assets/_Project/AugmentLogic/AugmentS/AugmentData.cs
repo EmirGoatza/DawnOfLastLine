71475f33db45d08428b0480d7d978f398fcfd1ce
Kyutsune <jaipechofizz@gmail.com>
2026-02-14 18:47:08 +0100

    Ajout de la notion de rareté dans les cartes

--- Assets/_Project/AugmentLogic/AugmentS/AugmentData.cs
+++ Assets/_Project/AugmentLogic/AugmentS/AugmentData.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
 
+public enum Rarity
+{
+    Common = 50,
+    Rare = 20,
+    Epic = 5,
+    Legendary = 1
+}
+
 [CreateAssetMenu(menuName = "Augment/New Augment")]
 public class AugmentData : ScriptableObject
 {
     public string augmentName;
     public string description;
     public AugmentEffect effect;
-    // public Rarity rarity;
+    public Rarity rarity;
 }
